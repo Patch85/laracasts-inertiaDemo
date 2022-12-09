e88cfe77ac880708cb5a2860ed94006b7eeb9cd8
Dillon John <dillon.john@techflex.com>
2022-12-09 14:21:51 -0500

    Ep 03 - Pages

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+    name: String,
+    frameworks: Array
+})
+</script>
+
+<template>
+    <h1 class="text-bold text-5xl">Hello, {{ name }}!</h1>
+
+    <p class="text-sm my-2">In this series, the following frameworks will be used:</p>
+
+    <ul class="list">
+        <li
+            class="list-item"
+            v-for="framework in frameworks"
+            v-text="framework"
+        ></li>
+    </ul>
+</template>
