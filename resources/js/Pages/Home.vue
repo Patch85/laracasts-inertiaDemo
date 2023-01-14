a4e6f2b4e965fcc67a1065f87e9b06f89a845c7d
Dillon A. John <dillon.a.john@gmail.com>
2023-01-14 11:26:02 -0500

    Ep 15 - Dyname Title and Meta Tags
    - Globally register the Head component and use it on each Vue page to
      set the title for each

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -5,6 +5,9 @@ import Layout from './Shared/Layout.vue';
 </script>
 
 <template>
+
+    <Head title="Home" />
+
     <Layout>
         <template #title>
             <h1 class="text-bold text-4xl text-slate-200 font-bold">
--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -13,22 +13,20 @@ defineProps({
 </script>
 
 <template>
+
+    <Head title="Users" />
+
     <Layout>
         <template #title>
             <h1 class="text-bold text-4xl font-bold text-slate-200">Users</h1>
         </template>
     </Layout>
 
-
-    <div
-        style="margin-top: 1400px"
-        class="text-slate-200"
-    >
+    <div style="margin-top: 1400px"
+         class="text-slate-200">
         <p>The current time is {{ time }}</p>
-        <Link
-            href="/users"
-            class="text-blue-500"
-            preserve-scroll
-        >Refresh</Link>
+        <Link href="/users"
+              class="text-blue-500"
+              preserve-scroll>Refresh</Link>
     </div>
 </template>
