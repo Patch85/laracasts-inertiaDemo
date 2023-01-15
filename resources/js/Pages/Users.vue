372497f1fa0e8bdfdd3b80b83d8efcb5f5f1d3a3
Dillon A. John <dillon.a.john@gmail.com>
2023-01-14 21:31:38 -0500

    WIP

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -17,13 +17,13 @@ defineProps({ users: Array })
             <h1 class="text-bold text-4xl font-bold text-slate-200">Users</h1>
         </template>
         <template #main>
-        
-            <ul class="mt-6 ml-4">
-                <li v-for="user in users"
-                    :key="user.id"
-                    v-text="user.name"
-                    class="text-md text-slate-200"></li>
-            </ul>
+            <table class="mt-6 ml-4">
+                <tr v-for="user in users"
+                    class="text-md text-slate-200">
+                    <td v-text="user.name" />
+                    <td>edit</td>
+                </tr>
+            </table>
         </template>
     </Layout>
 </template>
